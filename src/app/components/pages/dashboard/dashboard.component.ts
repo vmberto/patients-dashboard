2809419f9899767842273375aa5ba23f892f3629
Umberto <umbertobarros@mangue3.com>
2019-01-11 10:12:53 -0300

    [master] | dashboard controller refactor

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -13,17 +13,24 @@ import * as moment from 'moment';
     styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-    public patientsCounter: Observable<any>;
-    public myChart = [];
+    
+    // Counters
+    public patientsCounter: Observable<number>;
+
+    // Chart patients x health-insurance relation
+    public patientsHealthInsuranceChart = [];
+    public healthInsuranceLabels: string[];
+    public patientsHealthInsuranceData: number[] = [];
+    public randomColors: string[] = [];
+
+    // Chart total sessions last week
     public lastWeekSessionsChart = [];
+    public totalHoursWorked: any;
+    public totalSessions: number;
 
-    public labels: string[];
-    public chartData: number[] = [];
-    public colors: string[] = [];
-
-    public totalHoursWorked;
-
-    constructor(private patientsService: PatientsService, private healthInsuranceService: HealthInsurancesService) { }
+    constructor(
+        private patientsService: PatientsService,
+        private healthInsuranceService: HealthInsurancesService) { }
 
 
 
@@ -31,25 +38,31 @@ export class DashboardComponent implements OnInit {
 
         this.patientsCounter = this.patientsService.getPatientsTotalCount().pipe(map(res => res.data));
 
+        this.patientsHealthInsuranceRelationChart();
+
+        this.totalSessionsLastWeekChart();
+
+    }
+
+    private patientsHealthInsuranceRelationChart() {
+
         this.healthInsuranceService.getPatientsRelation()
             .subscribe(
                 res => {
-                    this.labels = res.data.map(data => data.name);
-                    res.data.forEach((data) => {
-                        this.chartData.push(data.Patients.length);
-                    });
-                    this.labels.forEach(() => {
-                        this.colors.push(generateRandomColor());
-                    });
+                    this.healthInsuranceLabels = res.data.map(data => data.name);
 
-                    this.myChart = new Chart('myChart', {
+                    this.patientsHealthInsuranceData = res.data.map(data => data.Patients.length);
+
+                    this.randomColors = this.healthInsuranceLabels.map(_data => _data = generateRandomColor());
+                    
+                    this.patientsHealthInsuranceChart = new Chart('patientsHealthInsuranceChart', {
                         type: 'doughnut',
                         data: {
-                            labels: this.labels,
+                            labels: this.healthInsuranceLabels,
                             datasets: [{
-                                data: this.chartData,
-                                backgroundColor: this.colors,
-                                borderColor: this.colors,
+                                data: this.patientsHealthInsuranceData,
+                                backgroundColor: this.randomColors,
+                                borderColor: this.randomColors,
                                 borderWidth: 1
                             }]
                         },
@@ -67,6 +80,9 @@ export class DashboardComponent implements OnInit {
                     });
                 });
 
+    }
+
+    private totalSessionsLastWeekChart() {
 
         let startDate: any = moment().startOf('week').subtract(1, 'week');
         let endDate: any = moment().endOf('week').subtract(1, 'week');
@@ -78,9 +94,11 @@ export class DashboardComponent implements OnInit {
         this.patientsService.getLastWeekSessions(lastWeek).subscribe(
             res => {
                 const sessionsNumber = this.enumerateDaysBetweenDates(startDate, endDate).map(day => day = this.getNumbers(res.data, day));
-                
+                this.totalSessions = res.data.length;
+
                 this.lastWeekSessionsChart = new Chart('lastWeekSessionsChart', {
                     type: 'line',
+                    beginAtZero: true,
                     data: {
                         labels: this.enumerateDaysBetweenDates(startDate, endDate).map(date => moment(date).format('DD/MM/YYYY')),
                         datasets: [{
@@ -90,21 +108,30 @@ export class DashboardComponent implements OnInit {
                         }]
                     },
                     options: {
+                        responsive: true,
                         legend: {
                             display: false,
                         },
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true,
+                                    callback: function (value) { if (Number.isInteger(value) && value >= 0) { return value; } },
+                                    stepSize: 1
+                                }
+                            }]
+                        }
                     }
                 });
             }
         )
-
     }
 
-    public getNumbers(sessions, day) {
+    private getNumbers(sessions, day) {
         return sessions.filter(session => moment(session.attendance_at).format('YYYY-MM-DD') === moment(day).format('YYYY-MM-DD')).length;
     };
 
-    public enumerateDaysBetweenDates(startDate, endDate) {
+    private enumerateDaysBetweenDates(startDate, endDate) {
         var dates = [],
             currentDate = startDate,
             addDays = function (days) {
