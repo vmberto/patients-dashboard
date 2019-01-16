f5c74787176559902c4d149051590fad39ac65be
Umberto <umbertobarrosf@gmail.com>
2019-01-16 18:42:23 -0300

    [master] | patient data with anamnesis download

--- src/app/components/pages/patients/patients-show-wrapper/patient-data/patient-data.component.ts
+++ src/app/components/pages/patients/patients-show-wrapper/patient-data/patient-data.component.ts
@@ -1,4 +1,6 @@
+import { AnamnesisService } from './../../../../../services/entities/anamnesis.service';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import saveAs from 'node_modules/file-saver';
 
 @Component({
   selector: 'app-patient-data',
@@ -10,14 +12,14 @@ export class PatientDataComponent implements OnInit {
   @Input() patientData;
   @Output() editPatientData: EventEmitter<any> = new EventEmitter<any>();
 
+  public downloadingAnamnesis: boolean;
   public openInput = false;
   public edit: string;
 
-  constructor() { }
+  constructor(private anamnesisService: AnamnesisService) { }
 
   ngOnInit() {
     this.edit = this.patientData.name;
-
   }
 
   public openInputToEdit() {
@@ -30,6 +32,26 @@ export class PatientDataComponent implements OnInit {
       this.editPatientData.emit(this.patientData);
     }
     this.openInput = false;
+  }
+
+  public downloadPatientAnamnesis() {
+
+    this.downloadingAnamnesis = true;
+
+    /** @TODO colocar um seletor para definir a anamnese que será aplicado os dados do paciente */
+    this.anamnesisService.downloadAnamnesis(1, this.patientData)
+      .then((res) => {
+        this.downloadingAnamnesis = false;
+        const file = new Blob([res], { type: 'application/pdf' });
+        const anamnesisName = this.patientData.name.toLowerCase().split(' ').join('-');
+        const filename = `anamnese-${anamnesisName}.pdf`;
+
+        saveAs(file, filename);
+      })
+      .catch((err) => {
+        this.downloadingAnamnesis = false;
+        console.log(err);
+      });
   }
 
 }
